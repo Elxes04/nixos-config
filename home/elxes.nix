2510172a42cb20d4d829c118e93deb1bd85d1284
elxes <your.email@example.com>
2026-01-24 05:59:32 +0100

    Update flake dependencies and configurations

--- home/elxes.nix
+++ home/elxes.nix
@@ -65,11 +65,25 @@
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
-    oh-my-zsh = {
-      enable = true;
-      plugins = [ "git" "docker" "kubectl" "npm" "python" "rust" ];
-      theme = "robbyrussell";
-    };
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = pkgs.writeTextDir "share/zsh/plugins/powerlevel10k-config/powerlevel10k-config.zsh" ''
+          # Powerlevel10k configuration
+          if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+            source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+          fi
+          
+          [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+        '';
+        file = "share/zsh/plugins/powerlevel10k-config/powerlevel10k-config.zsh";
+      }
+    ];
 
     shellAliases = {
       ll = "eza -la";
