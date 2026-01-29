21a5ecc82e11cba824c96606f7489f2452b4d51d
elxes <your.email@example.com>
2026-01-29 06:23:20 +0100

    added examine

--- flake.nix
+++ flake.nix
@@ -44,6 +44,7 @@
             ./modules/system/filesystems.nix
             ./modules/system/fonts.nix
             ./modules/desktop/cosmic.nix
+            ./modules/desktop/examine.nix
             ./modules/games/minecraft.nix
             ./modules/packages/gaming.nix
             ./modules/packages/development.nix
