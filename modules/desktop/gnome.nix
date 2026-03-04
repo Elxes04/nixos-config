edc8384a6bc27f080357464fa9bd238e4c08f5e3
Elxes04 <124086168+Elxes04@users.noreply.github.com>
2026-03-04 03:00:19 +0100

    updated

---
+++ modules/desktop/gnome.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ... };
+
+{  
+   services.displayManager.gdm.enable = true;
+
+   service.desktopManager.gnome.enable = true;
+
+   services.gnome.core-apps.enable = true;
+
+   services.gnome.core-developer-tools.enable = false;
+
+   services.gnome.games.enable = false;
+
+}
