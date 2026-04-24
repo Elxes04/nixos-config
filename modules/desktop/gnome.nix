b37343df3e33cf8d2fc4551f30ce0e9b2ec13e92
Elxes04 <124086168+Elxes04@users.noreply.github.com>
2026-04-24 23:13:05 +0200

    updated

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -1,22 +1,21 @@
 { config, pkgs, ... }:
 
-{  
-   services.displayManager.gdm.enable = true;
+{
+  services.xserver.enable = true;
 
-   services.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
-   services.gnome.core-apps.enable = true;
+  services.gnome.core-apps.enable = true;
+  services.gnome.core-developer-tools.enable = false;
+  services.gnome.games.enable = false;
 
-   services.gnome.core-developer-tools.enable = false;
-
-   services.gnome.games.enable = false;
-
-   environment.systemPackages = with pkgs; [
-      gnomeExtensions.arcmenu
-      gnomeExtensions.appindicator
-      gnomeExtensions.blur-my-shell
-      gnomeExtensions.dash-to-dock
-      gnomeExtensions.system-monitor
-      gnomeExtensions.xwayland-indicator
-   ];
+  environment.systemPackages = with pkgs; [
+    unstable.gnomeExtensions.arcmenu
+    unstable.gnomeExtensions.appindicator
+    unstable.gnomeExtensions.blur-my-shell
+    unstable.gnomeExtensions.dash-to-dock
+    unstable.gnomeExtensions.system-monitor
+    unstable.gnomeExtensions.xwayland-indicator
+  ];
 }
--- modules/packages/desktop-apps.nix
+++ modules/packages/desktop-apps.nix
@@ -1,5 +1,16 @@
 { config, pkgs, ... }:
 
+let
+  optimizedEnv = old: {
+    env = (old.env or {}) // {
+      NIX_CFLAGS_COMPILE = (old.env.NIX_CFLAGS_COMPILE or "") + " -O3 -march=native -mtune=native";
+      NIX_CXXFLAGS_COMPILE = (old.env.NIX_CXXFLAGS_COMPILE or "") + " -O3 -march=native -mtune=native";
+    };
+  };
+
+  handbrake-native = pkgs.handbrake.overrideAttrs (old: (optimizedEnv old) // { pname = "handbrake-native"; });
+  ffmpeg-native = pkgs.ffmpeg.overrideAttrs (old: (optimizedEnv old) // { pname = "ffmpeg-native"; doCheck = false; });
+in
 {
   environment.systemPackages = with pkgs; [
     firefox
@@ -24,6 +35,8 @@
     gnome-software
     flameshot
     obs-studio
+    handbrake-native
+    ffmpeg-native
     wayland-utils
     wl-clipboard
     xclip
@@ -32,6 +45,7 @@
     virt-manager
     rustdesk
     easyeffects
+    virt-viewer
   ];
 }
 
